721092c5b27056549ef88a857cb276e0200fb935
moldenburger <maritoldenburger@hotmail.com>
2025-02-18 15:48:06 +0100

    Finished form

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import React, {useState} from "react";
+import {useForm} from 'react-hook-form';
 
 function App() {
 
@@ -7,6 +8,17 @@ function App() {
     const [bananas, setBananas] = useState(0);
     const [apples, setApples] = useState(0);
     const [kiwis, setKiwis] = useState(0);
+
+    const {register, handleSubmit} = useForm({
+        defaultValues: {
+            age: 0,
+            "delivery-time": "day"
+        }
+    });
+
+    function handleFormSubmit(data) {
+        console.log(data);
+    }
 
     function resetButton() {
         setStrawberries(0)
@@ -66,6 +78,112 @@ function App() {
                 <article>
                     <button type="button" onClick={() => resetButton()}>Reset</button>
                 </article>
+
+                <form onSubmit={handleSubmit(handleFormSubmit)}>
+                    <label htmlFor="first-name-field">
+                        Voornaam
+                        <input
+                            type="text"
+                            id="first-name-field"
+                            {...register("first-name")}
+                            // value={firstName}
+                            // onChange={(e) => setFirstName(e.target.value)}
+                        />
+                    </label>
+                    <label htmlFor="last-name-field">
+                        Achternaam
+                        <input
+                            type="text"
+                            id="last-name-field"
+                            {...register("last-name")}
+                            // value={lastName}
+                            // onChange={(e) => setLastName(e.target.value)}
+                        />
+                    </label>
+                    <label htmlFor="age-field">
+                        Leeftijd
+                        <input
+                            type="number"
+                            id="age-field"
+                            {...register("age")}
+                            // value={age}
+                            // onChange={(e) => setAge(e.target.value)}
+                        />
+                    </label>
+                    <label htmlFor="zipcode-field">
+                        Postcode
+                        <input
+                            type="text"
+                            id="zipcode-field"
+                            {...register("zipcode")}
+                            // value={zipcode}
+                            // onChange={(e) => setZipcode(e.target.value)}
+                        />
+                    </label>
+                    <label htmlFor="delivery-frequency-field">
+                        Bezorgfrequentie
+                        <select
+                            id="delivery-frequency-field"
+                            {...register("delivery-frequency")}
+                            // value={zipcode}
+                            // onChange={(e) => setZipcode(e.target.value)}
+                        >
+                            <option value="weekly">Iedere week</option>
+                            <option value="bi-weekly">Om de week</option>
+                            <option value="monthly">Iedere maand</option>
+                        </select>
+                    </label>
+                    <div className="delivery-time-field">
+                        <label htmlFor="day">
+                            <input
+                                type="radio"
+                                id="day"
+                                value="day"
+                                name="delivery-time"
+                                {...register("delivery-time")}
+                                // value={zipcode}
+                                // onChange={(e) => setZipcode(e.target.value)}
+                            />
+                            Overdag
+                        </label>
+                        <label htmlFor="night">
+                            <input
+                                type="radio"
+                                id="night"
+                                value="night"
+                                name="delivery-time"
+                                {...register("delivery-time")}
+                                // value={zipcode}
+                                // onChange={(e) => setZipcode(e.target.value)}
+                            />
+                            's Avonds
+                        </label>
+                    </div>
+                    <label htmlFor="message-field">
+                        Opmerking
+                        <textarea
+                            id="message-field"
+                            rows="4"
+                            cols="40"
+                            {...register("message")}
+                            // value={zipcode}
+                            // onChange={(e) => setZipcode(e.target.value)}
+                        ></textarea>
+                    </label>
+                    <label htmlFor="terms-and-conditions-field">
+                        <input
+                            type="checkbox"
+                            id="terms-and-conditions-field"
+                            {...register("terms-and-conditions")}
+                            // checked={termsAndConditions}
+                            // onChange={() => toggleTermsAndConditions(!termsAndConditions)}
+                        />
+                        Ik ga akkoord met de voorwaarden
+                    </label>
+                    <button type="submit">
+                        Verzend
+                    </button>
+                </form>
             </main>
         </>
     )
