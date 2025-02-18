05e44b9791af12c87e8882c2c4a1790d5b7084a2
moldenburger <maritoldenburger@hotmail.com>
2025-02-18 12:32:51 +0100

    Finished fruit counters

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,74 @@
-
 import './App.css'
+import React, {useState} from "react";
 
 function App() {
 
-  return (
-    <>
-        <h1>Fruitmand bezorgservice</h1>
-    </>
-  )
+    const [strawberries, setStrawberries] = useState(0);
+    const [bananas, setBananas] = useState(0);
+    const [apples, setApples] = useState(0);
+    const [kiwis, setKiwis] = useState(0);
+
+    function resetButton() {
+        setStrawberries(0)
+        setBananas(0);
+        setApples(0);
+        setKiwis(0);
+    }
+
+    return (
+        <>
+            <main>
+                <h1>Fruitmand bezorgservice</h1>
+                <article>
+                    <h2>🍓 Aardbeien</h2>
+                    <button type="button"
+                            disabled={strawberries === 0}
+                            onClick={() => setStrawberries(strawberries - 1)}>-
+                    </button>
+                    <p>{strawberries}</p>
+                    <button type="button"
+                            onClick={() => setStrawberries(strawberries + 1)}>+
+                    </button>
+                </article>
+                <article>
+                    <h2>🍌 Bananen</h2>
+                    <button type="button"
+                            disabled={bananas === 0}
+                            onClick={() => setBananas(bananas - 1)}>-
+                    </button>
+                    <p>{bananas}</p>
+                    <button type="button"
+                            onClick={() => setBananas(bananas + 1)}>+
+                    </button>
+                </article>
+                <article>
+                    <h2>🍏 Appels</h2>
+                    <button type="button"
+                            disabled={apples === 0}
+                            onClick={() => setApples(apples - 1)}>-
+                    </button>
+                    <p>{apples}</p>
+                    <button type="button"
+                            onClick={() => setApples(apples + 1)}>+
+                    </button>
+                </article>
+                <article>
+                    <h2>🥝 Kiwi's</h2>
+                    <button type="button"
+                            disabled={kiwis === 0}
+                            onClick={() => setKiwis(kiwis - 1)}>-
+                    </button>
+                    <p>{kiwis}</p>
+                    <button type="button"
+                            onClick={() => setKiwis(kiwis + 1)}>+
+                    </button>
+                </article>
+                <article>
+                    <button type="button" onClick={() => resetButton()}>Reset</button>
+                </article>
+            </main>
+        </>
+    )
 }
 
 export default App
